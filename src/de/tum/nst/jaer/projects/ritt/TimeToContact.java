698ac9310bab18916e9d3de7ff4ab78c99537447
Konstantin Ritt <ritt@mytum.de>
2017-08-21 17:23:28 +0200

    loading of ground truth FOE data implemented

--- src/de/tum/nst/jaer/projects/ritt/TimeToContact.java
+++ src/de/tum/nst/jaer/projects/ritt/TimeToContact.java
@@ -10,13 +10,22 @@ import ch.unizh.ini.jaer.projects.rbodo.opticalflow.LocalPlanesFlow;
 import ch.unizh.ini.jaer.projects.rbodo.opticalflow.LucasKanadeFlow;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.Vector;
+import java.util.logging.Level;
+import javax.swing.JFileChooser;
 import net.sf.jaer.Description;
 import net.sf.jaer.DevelopmentStatus;
 import net.sf.jaer.chip.AEChip;
 import net.sf.jaer.event.ApsDvsEventPacket;
 import net.sf.jaer.event.EventPacket;
 import net.sf.jaer.event.orientation.ApsDvsMotionOrientationEvent;
+import static net.sf.jaer.eventprocessing.EventFilter.log;
 import net.sf.jaer.eventprocessing.EventFilter2D;
 import net.sf.jaer.graphics.FrameAnnotater;
 import net.sf.jaer.util.DrawGL;
@@ -56,23 +65,34 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
     private double mProbMax;
     // coordinates of maximum probability: location of FOE
     private int foeX, foeY;
+    // time constant [microsec] for probability decay of FOE position
+    private float tProb = getFloat("tProb", 1000f);
 
     //  ______filtering incoming events
     // valid flow vector angle in degrees, +- vector to center from current pixel
-    private final double validAngle = 45 * Math.PI / 180.0;
+    private final double validAngle = 55 * Math.PI / 180.0;
+    // turn diverging field around center filter on/off
+    private boolean centralFilter = getBoolean("centralFilter", true);
     // center coordinates of current sensor
     private int centerX, centerY;
     // radius of evaluated pixels around current foe for probability update
-    private int updateR;
+    private int updateR = getInt("updateR", 50);
+    
+    // _______ground truth related
+    // variable that gives availability of ground truth data
+    private boolean importedGTfromFile = false;
+    // ground truth values:
+    private Vector<Double> gtTime;
+    private Vector<Float> gtFoeX, gtFoeY;
 
     //_______________Variables handling optical flow estimation_________________
     private EventPacket flowPacket;
     private final AbstractMotionFlow flowFilter;
 
     //____________________Variables for user handling___________________________
+    
     private boolean displayFOE = getBoolean("displayFOE", true);
     private boolean displayRawInput = getBoolean("displayRawInput", true);
-    private float tProb = getFloat("tProb", 1000f);
 
     //_______________Variables for timing and performance_______________________
     private int filteredOutSpeed = 0;  // count events that are being omitted
@@ -83,15 +103,19 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
         super(chip);
 
         // configure enclosed filter that creates optical flow events
-        flowFilter = new LocalPlanesFlow(chip);//new LucasKanadeFlow(chip); //TODO: make adaptable to other optical flow algos
+        flowFilter = new LucasKanadeFlow(chip); // new LocalPlanesFlow(chip);//TODO: make adaptable to other optical flow algos
         flowFilter.setDisplayRawInput(false); // to pass flow events not raw in        
         setEnclosedFilter(flowFilter);
 
         resetFilter();
 
+        setPropertyTooltip("importGTfromFile", "imports text file to load ground truth FOE data");
+        setPropertyTooltip("resetGroundTruth", "Resets the ground truth focus of expansion loaded from file.");
         setPropertyTooltip("Focus of Expansion", "tProb", "time constant [microsec]"
                 + " for probability decay of FOE position");
+        setPropertyTooltip("Focus of Expansion", "updateR", "radius [pixels] around current FOE where probability is updated");
         setPropertyTooltip("Focus of Expansion", "displayFOE", "shows the estimated focus of expansion (FOE)");
+        setPropertyTooltip("Focus of Expansion", "centralFilter", "turn filter for diverging field around center on/off");
         setPropertyTooltip("View", "displayRawInput", "shows the input events, instead of the motion types");
 
     }
@@ -131,7 +155,7 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
             vx = ein.velocity.x;
             vy = ein.velocity.y;
 
-            if (getRelAngle(x - centerX, y - centerY, vx, vy) > validAngle) {
+            if (centralFilter && getRelAngle(x - centerX, y - centerY, vx, vy) > validAngle) {
                 ein.setFilteredOut(true);
                 filteredOutCentral++;
                 continue;
@@ -175,8 +199,10 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
                 }
             }
         }// end while(i.hasNext())
-
-        System.out.println("speed = " + filteredOutSpeed + " central = " + filteredOutCentral);
+        //int currentGTIndex = binarySearch(ts*1e-6);
+        //System.out.println("speed = " + filteredOutSpeed + " central = " + filteredOutCentral + " foeX = " + foeX + " foeY = " + foeY
+        //+ " corresponding gt foeX=" + gtFoeX.get(currentGTIndex) + " foeY=" +gtFoeY.get(currentGTIndex) + " at time ts=" + ts
+        //+ " with index " + currentGTIndex);
         return isDisplayRawInput() ? in : flowPacket;
     }
 
@@ -203,6 +229,8 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
         mTime = new int[sizex][sizey];
         centerX = sizex / 2 - 1;
         centerY = sizey / 2 - 1;
+        foeX = centerX;
+        foeY = centerY;
     }
 
     /**
@@ -213,14 +241,101 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
     public final void initFilter() {
         resetFilter();
     }
+    
+    
+    // Allows importing two 2D-arrays containing the x-/y- components of the 
+    // motion flow field used as ground truth.
+    synchronized public void doImportGTfromFile() {      
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Choose ground truth file");
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.setMultiSelectionEnabled(false);
+                
+        gtTime = new Vector<Double>();
+        gtFoeX = new Vector<Float>();
+        gtFoeY = new Vector<Float>();
+        BufferedReader reader = null;
+        File file = null;
+        if (chooser.showOpenDialog(chip.getAeViewer().getFilterFrame()) == JFileChooser.APPROVE_OPTION) {
+            try {                
+                file = new File(chooser.getSelectedFile().getPath());                
+
+                log.info("Imported ground truth file");
+                reader = new BufferedReader(new FileReader(file));
+                String text = null;
+
+                while ((text = reader.readLine()) != null) {
+                    String[] columns = text.split(" ");
+                    if (columns[0].matches("#")) {
+                        log.info("commented line removed");
+                        continue;
+                    }
+                    gtTime.add(Double.parseDouble(columns[0]));
+                    gtFoeX.add((float) Double.parseDouble(columns[22]));
+                    gtFoeY.add((float) Double.parseDouble(columns[23]));            
+                }
+                importedGTfromFile = true;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                log.log(Level.SEVERE, null, e);
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                }
+            }           
+        }
+        log.info("import of ground truth done, data with times from "+gtTime.firstElement()+" to "+gtTime.lastElement());             
+    }
+    
+    void resetGroundTruth() {
+        importedGTfromFile = false;
+        gtTime = null;
+        gtFoeX = null;
+        gtFoeY = null;
+    }
+
+    synchronized public void doResetGroundTruth() {
+        resetGroundTruth();
+    }
+    
+    public float getTProb() {
+        return this.tProb;
+    }
+
+    public void setTProb(final float tProb_) {
+        this.tProb = tProb_;
+        putFloat("tProb", tProb_);
+    }
+    
+    public int getUpdateR() {
+        return this.updateR;
+    }
+
+    public void setUpdateR(final int updateR_) {
+        this.updateR = updateR_;
+        putInt("updateR", updateR_);
+    }
 
     public boolean isDisplayFOE() {
         return displayFOE;
     }
 
-    public void setDisplayFOE(boolean displayFOE) {
-        this.displayFOE = displayFOE;
-        putBoolean("displayFOE", displayFOE);
+    public void setDisplayFOE(boolean displayFOE_) {
+        this.displayFOE = displayFOE_;
+        putBoolean("displayFOE", displayFOE_);
+    }
+    
+    public boolean isCentralFilter() {
+        return centralFilter;
+    }
+
+    public void setCentralFilter(boolean centralFilter_) {
+        this.centralFilter = centralFilter_;
+        putBoolean("centralFilter", centralFilter_);
     }
 
     public boolean isDisplayRawInput() {
@@ -230,15 +345,6 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
     public void setDisplayRawInput(boolean displayRawInput) {
         this.displayRawInput = displayRawInput;
         putBoolean("displayRawInput", displayRawInput);
-    }
-
-    public float getTProb() {
-        return this.tProb;
-    }
-
-    public void setTProb(final float tProb_) {
-        this.tProb = tProb_;
-        putFloat("tProb", tProb_);
     }
 
     @Override
@@ -256,6 +362,52 @@ public class TimeToContact extends EventFilter2D implements FrameAnnotater {
             DrawGL.drawCircle(gl, foeX, foeY, 4, 15);
             gl.glPopMatrix();
         }
+        
+        if (importedGTfromFile) {
+            if (!isFilterEnabled()) {
+                return;
+            }
+            GL2 gl = drawable.getGL().getGL2();
+            if (gl == null) {
+                return;
+            }
+            checkBlend(gl);
+            gl.glPushMatrix();
+            int currentGTIndex = binarySearch(ts*1e-6);
+            DrawGL.drawBox(gl, gtFoeX.get(currentGTIndex), gtFoeY.get(currentGTIndex), 4, 4, 0);
+            gl.glPopMatrix();
+        }
+    }
+    
+    // returns the index of the timestamp in GT data closest to current time
+    private int binarySearch(double time) {
+        if (!importedGTfromFile) {
+            return 0;
+        }
+        
+        if(time < gtTime.firstElement()) {
+            return 0;
+        }
+        if(time > gtTime.lastElement()) {
+            return gtTime.size()-1;
+        }
+
+        int lo = 0;
+        int hi = gtTime.size() - 1;
+
+        while (lo <= hi) {
+            int mid = (hi + lo) / 2;
+
+            if (time < gtTime.get(mid)) {
+                hi = mid - 1;
+            } else if (time > gtTime.get(mid)) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        // lo == hi + 1
+        return (gtTime.get(lo) - time) < (time - gtTime.get(hi)) ? lo : hi;
     }
 
 }
